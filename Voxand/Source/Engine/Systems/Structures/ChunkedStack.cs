dac343631a479ad1f31ea84e0f4f101ef7e350d1
Tile37 <mykola.kondur39@gmail.com>
2025-05-29 02:46:14 +0300

    Added chunked stack to be used in voxel brick soft deletion.

---
+++ Voxand/Source/Engine/Systems/Structures/ChunkedStack.cs
@@ -0,0 +1,56 @@
+﻿namespace Voxand.Engine.Systems.Structures;
+public class ChunkedStack<T>
+{
+    LinkedList<T[]> linkedList;
+    int chunkCapacity;
+    LinkedListNode<T[]> lastChunk;
+    int indexOfNext = 0;
+    int Count = 0;
+    
+    public ChunkedStack(int chunkCapacity)
+    {
+        this.chunkCapacity = chunkCapacity;
+        T[] chunk = new T[chunkCapacity];
+        lastChunk = new(chunk);
+        linkedList = [];
+        linkedList.AddLast(lastChunk);
+    }
+
+    public void Push(T item)
+    {
+        if (indexOfNext == chunkCapacity)
+        {
+            T[] newChunk = new T[chunkCapacity];
+            newChunk[0] = item;
+            lastChunk = new(newChunk);
+            linkedList.AddLast(lastChunk);
+            indexOfNext = 1;
+        }
+        else
+        {
+            lastChunk.Value[indexOfNext] = item;
+            indexOfNext++;
+        }
+        Count++;
+    }
+
+    public T Pop()
+    {
+        if (Count == 0)
+            throw new InvalidOperationException("Chunked stack empty.");
+
+        T result = lastChunk.Value[indexOfNext - 1];
+
+        if (indexOfNext - 1 == 0)
+        {
+            indexOfNext = chunkCapacity;
+            lastChunk = lastChunk.Previous ?? lastChunk;
+        }
+        else
+        {
+            indexOfNext--;
+        }
+        Count--;
+        return result;
+    }
+}
